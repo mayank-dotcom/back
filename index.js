1c61f336ade98bd627c1293094aa65a931a0086e
mayank-dotcom <mayankmishra11d@gmail.com>
2024-12-26 11:34:57 +0530

    first commit

--- index.js
+++ index.js
@@ -25,7 +25,24 @@ const verifyToken = (req, res, next) => {
   }
 };
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://att-db.onrender.com'], // Replace your-frontend-url with your actual frontend URL
+  origin: function(origin, callback){
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    
+    const allowedOrigins = [
+      'http://localhost:5173', 
+      'https://ats.centennialinfotech.com',
+      'http://ats.centennialinfotech.com'
+    ];
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 // Middleware configurations
