abadd9a8069cd08e67e65a0eaf39ac1634422b37
mayank-dotcom <mayankmishra11d@gmail.com>
2024-12-26 12:21:14 +0530

    first commit

--- index.js
+++ index.js
@@ -25,22 +25,7 @@ const verifyToken = (req, res, next) => {
   }
 };
 app.use(cors({
-  origin: function(origin, callback){
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    
-    const allowedOrigins = [
-      'http://localhost:5173', 
-      'https://ats.centennialinfotech.com',
-      'http://ats.centennialinfotech.com'
-    ];
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
